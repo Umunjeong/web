487d0b2dc4a38d4647b564307aed51091e0c2b3a
qpaseo <qazcgik@gmail.com>
2025-01-20 01:40:35 +0900

    update : update name 1:32

--- src/pages/Pin/UpdatePin/index.jsx
+++ src/pages/Pin/UpdatePin/index.jsx
@@ -10,7 +10,7 @@ import {
   From_else_Box,
 } from "../../../styles/updatePin";
 
-import Header from "../../../Components/Header/Header";
+import Header from "../../../Components/Header/Header.jsx";
 import All_check from "../../../Components/Page/Check_Bar/Check_Ber";
 
 import Pin_input from "../../../Components/Pin/Input/Input";
